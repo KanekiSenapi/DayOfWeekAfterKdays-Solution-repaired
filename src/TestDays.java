69f8e07f04f9cc6169004a8ea70524fe247e00c7
Kaneki #Senpai <v.kaneki.senpai@gmail.com>
2019-06-16 03:10:45 +0200

    add test 300 days for every day of week

--- src/TestDays.java
+++ src/TestDays.java
@@ -2,6 +2,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 
 public class TestDays {
 
@@ -45,6 +52,27 @@ public class TestDays {
         String except = "Wed";
         String get = Solution.solution("Fri", 5);
         Assertions.assertEquals(except,get);
+    }
+
+    @Test
+    public void Test300DaysEveryDay() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar c = Calendar.getInstance();
+        c.setTime(sdf.parse("01-07-2019"));
+        for(int i = 0 ; i< 7 ; i++) {
+            String KK = x(c.get(Calendar.DAY_OF_WEEK));
+            for (int k = 0; k < 300; k++) {
+                String get = Solution.solution(KK, k);
+                String exc = x(c.get(Calendar.DAY_OF_WEEK));
+                Assertions.assertEquals(exc, get);
+                c.set(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) + 1);
+            }
+        }
+    }
+
+    private String x(int k){
+        List<String> DAYS = Arrays.asList("0","Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+        return DAYS.get(k);
     }
 
 }
